76af6cea88729c98db27762ac6071a0bc994c0a9
MERO-DA <82920463+MERO-DA@users.noreply.github.com>
2021-05-06 21:16:58 +0300

    Add files via upload

--- MERO.lua
+++ MERO.lua
@@ -17,7 +17,7 @@ print([[
 ]])
 io.popen("mkdir File_Bot") 
 io.popen("cd File_Bot && rm -rf commands.lua") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/MERO-DA/Files_MERO/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/MERO-DA/Files_Mero/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -3734,7 +3734,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevMEROW(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/MERO-DA/Files_MERO/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/MERO-DA/Files_Mero/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -3772,7 +3772,7 @@ t = " *⌯︙الملف »* "..file.."\n*⌯︙تم تعطيل ملف* \n"
 else
 t = " *⌯︙بالتاكيد تم تعطيل ملف →* "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MERO-DA/Files_MERO/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/MERO-DA/Files_Mero/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3792,7 +3792,7 @@ t = " *⌯︙بالتاكيد تم تفعيل ملف →* "..file.." \n"
 else
 t = " *⌯︙الملف »* "..file.."\n*⌯︙تم تفعيل ملف* \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MERO-DA/Files_MERO/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/MERO-DA/Files_Mero/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
